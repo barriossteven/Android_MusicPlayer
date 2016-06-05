a2239b954b0da57bad2e471776d3827a9c6637b6
barriossteven <barriossteven@gmail.com>
2016-06-04 22:15:13 -0400

    added seekbar

--- app/src/main/java/com/example/barrios/musicplayer/AudioActivity.java
+++ app/src/main/java/com/example/barrios/musicplayer/AudioActivity.java
@@ -1,9 +1,13 @@
  package com.example.barrios.musicplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+
+import java.io.IOException;
 
  public class AudioActivity extends AppCompatActivity {
 
@@ -12,6 +16,8 @@ import android.widget.SeekBar;
      private Button stopB;
      private SeekBar seekBar;
      private int songID;
+     MediaPlayer mediaPlayer;
+     Boolean isPrepared;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +28,57 @@ import android.widget.SeekBar;
         pauseB = (Button)findViewById(R.id.pauseButton);
         stopB = (Button)findViewById(R.id.stopButton);
         seekBar = (SeekBar)findViewById(R.id.seekBar);
+        mediaPlayer = MediaPlayer.create(AudioActivity.this, R.raw.meghan_trainor_no);
+        isPrepared = true;
+        seekBar.setMax(mediaPlayer.getDuration());
 
-        //getSongName();
+       playB.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               if(!isPrepared) {
+                   try {
+                       mediaPlayer.prepare();
+                       isPrepared = true;
+                   } catch (IOException e) {
+                       e.printStackTrace();
+                   }
+               }
+               mediaPlayer.start();
+
+           }
+       });
+
+        pauseB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+
+        stopB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                isPrepared = false;
+            }
+        });
+
+
+
+
 
 
     }
 
     private String getSongName(){
-        return
+        return null;
     }
-
+    private void stopPlaying(){
+        if(mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+    }
 
 
 }
